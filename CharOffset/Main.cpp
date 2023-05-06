b0cef9549c014f42b0b9efddf0d84a22d9466405
Thomas McGinley <thomas.mcginley@sinclair.edu>
2023-05-06 11:48:39 -0400

    invalidCharacterSelection detection added, convert start to lower case, short driver for early test.

--- CharOffset/Main.cpp
+++ CharOffset/Main.cpp
@@ -3,6 +3,7 @@
 //CIS1202 N01
 
 #include<iostream>
+#include<cctype>
 
 using namespace std;
 
@@ -10,9 +11,9 @@ char character(char start, int offset);
 
 int main()
 {
+	bool loop = true;
 
-
-
+	character('A', 1);
 	cout << "\n\n";
 	system("pause");
 	return 0;
@@ -20,5 +21,29 @@ int main()
 
 char character(char start, int offset)
 {
+	int invalidCharacterSelection = 1;
+	int invalidRangeException = 2;
+	bool upper = false;
+	const int ASCII_LOW = 97, //a
+		ASCII_HIGH = 122; //z
+
+	if (!isalpha(start))
+	{
+		throw invalidCharacterSelection;
+	}
+
+	if (isupper(start))
+	{
+		upper = true;
+		start = (char)tolower(int(start)); //converts start to lower, for bounds detection purposes
+	}
+	cout << start;
+	cout << "\t" << offset << "\t";
+	start = int(start) + offset;
+	cout << start;
+
+
+
+
 	return ' ';
 }

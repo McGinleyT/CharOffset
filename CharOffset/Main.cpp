5650a78e6278c3e6dc1c53472d858ccdb2f19a23
Thomas McGinley <thomas.mcginley@sinclair.edu>
2023-05-06 10:52:19 -0400

    Add project files.

---
+++ CharOffset/Main.cpp
@@ -0,0 +1,24 @@
+//Thomas McGinley
+//06 May, 2023
+//CIS1202 N01
+
+#include<iostream>
+
+using namespace std;
+
+char character(char start, int offset);
+
+int main()
+{
+
+
+
+	cout << "\n\n";
+	system("pause");
+	return 0;
+}
+
+char character(char start, int offset)
+{
+	return ' ';
+}

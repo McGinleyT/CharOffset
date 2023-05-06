f9b788134f17bef0529de9682d064ed9023b0553
Thomas McGinley <thomas.mcginley@sinclair.edu>
2023-05-06 12:14:17 -0400

    Function, exceptions, driver loop fully built. Ready for testing and optimization;

--- CharOffset/Main.cpp
+++ CharOffset/Main.cpp
@@ -3,7 +3,6 @@
 //CIS1202 N01
 
 #include<iostream>
-#include<cctype>
 
 using namespace std;
 
@@ -11,10 +10,32 @@ char character(char start, int offset);
 
 int main()
 {
-	bool loop = true;
+	char startChar = 'A';
+	int offsetNum = 1;
 
-	character('A', 1);
-	cout << "\n\n";
+	for (int i = 0; i < 6; i++)
+	{
+		cout << "Enter a starting character: ";
+		cin >> startChar;
+		cout << "Enter offset amount: ";
+		cin >> offsetNum;
+
+		try
+		{
+			cout << "End letter: " << character(startChar, offsetNum) << "\n\n";
+
+		}
+		catch (int error)
+		{
+			cout << "Error no. " << error << ": ";
+			if (error == 1)
+				cout << startChar << " is not a letter.\n\n";
+			if (error == 2)
+				cout << "End letter out of bounds\n\n";
+		}
+	}
+
+
 	system("pause");
 	return 0;
 }
@@ -32,18 +53,23 @@ char character(char start, int offset)
 		throw invalidCharacterSelection;
 	}
 
-	if (isupper(start))
+	if (isupper(start))	//converts start to lower, for bounds detection purposes
 	{
 		upper = true;
-		start = (char)tolower(int(start)); //converts start to lower, for bounds detection purposes
+		start = (char)tolower(int(start));
 	}
-	cout << start;
-	cout << "\t" << offset << "\t";
-	start = int(start) + offset;
-	cout << start;
 
+	char end = int(start) + offset; //offset movement
 
+	if (int(end) < ASCII_LOW || int(end) > ASCII_HIGH) //bounds detection
+	{
+		throw invalidRangeException;
+	}
 
+	if (upper)	//return char to upper
+	{
+		end = (char)toupper(int(end));
+	}
 
-	return ' ';
+	return end;
 }
